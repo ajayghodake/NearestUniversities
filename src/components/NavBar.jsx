8b0a2d9d3985813fdf26cf727742dc4089b6f27d
Ajay Ghodake <it.ajayghodake@gmail.com>
2025-04-11 15:15:33 +0530

    navbar is Responsive now

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,64 @@
-import React from "react";
+// import React from "react";
+// import "./Navbar.css";
+// import logo from "./main_logo.83e8edcb.svg"; // Make sure this path is correct
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar">
+//       <a href="https://emversity.com/" className="logo">
+//         <img src={logo} alt="Logo" className="logo-svg" />
+//       </a>
+
+//       <div className="main_menu">
+//         <a href="tel:8080804980" className="phoneNumber">
+//           <img
+//             src="https://dev-landing-pages-bucket.s3.ap-south-1.amazonaws.com/emversity/phone-2.svg"
+//             alt="pnone_icon"
+//             className="phone-svg"
+//           />
+//           <p>80 80 80 49 80</p>
+//         </a>
+
+//         <a href="https://emversity.com/about-us" className="aboutus">
+//           <p>About Us</p>
+//         </a>
+
+//         <a href="https://emversity.com/about-us" className="aboutus">
+//           <p>More</p>
+//         </a>
+
+//         <a href="https://emversity.com/programs" className="ExploreCourses">Explore Courses</a>
+
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+import React, { useState } from "react";
 import "./Navbar.css";
-import logo from "./main_logo.83e8edcb.svg"; // Make sure this path is correct
+import logo from "./main_logo.83e8edcb.svg";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <nav className="navbar">
       <a href="https://emversity.com/" className="logo">
         <img src={logo} alt="Logo" className="logo-svg" />
       </a>
 
-      <div className="main_menu">
+      <div className={`main_menu ${menuOpen ? "open" : ""}`}>
         <a href="tel:8080804980" className="phoneNumber">
           <img
             src="https://dev-landing-pages-bucket.s3.ap-south-1.amazonaws.com/emversity/phone-2.svg"
-            alt="pnone_icon"
+            alt="phone_icon"
             className="phone-svg"
           />
           <p>80 80 80 49 80</p>
@@ -27,8 +72,18 @@ const Navbar = () => {
           <p>More</p>
         </a>
 
-        <a href="https://emversity.com/programs" className="ExploreCourses">Explore Courses</a>
+        <a
+          href="https://emversity.com/programs"
+          className="ExploreCourses"
+        >
+          Explore Courses
+        </a>
+      </div>
 
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${menuOpen ? "open" : ""}`}></div>
       </div>
     </nav>
   );
