1d98f1f84f793fd8784174a5d5156ea95b149bb3
Ajay Ghodake <it.ajayghodake@gmail.com>
2025-04-11 14:54:24 +0530

    Navbar Updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,6 +8,28 @@ const Navbar = () => {
       <a href="https://emversity.com/" className="logo">
         <img src={logo} alt="Logo" className="logo-svg" />
       </a>
+
+      <div className="main_menu">
+        <a href="tel:8080804980" className="phoneNumber">
+          <img
+            src="https://dev-landing-pages-bucket.s3.ap-south-1.amazonaws.com/emversity/phone-2.svg"
+            alt="pnone_icon"
+            className="phone-svg"
+          />
+          <p>80 80 80 49 80</p>
+        </a>
+
+        <a href="https://emversity.com/about-us" className="aboutus">
+          <p>About Us</p>
+        </a>
+
+        <a href="https://emversity.com/about-us" className="aboutus">
+          <p>More</p>
+        </a>
+
+        <a href="https://emversity.com/programs" className="ExploreCourses">Explore Courses</a>
+
+      </div>
     </nav>
   );
 };
